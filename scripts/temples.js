1cc6fc590ad7560af0fec310b6765676b13200fb
kacharles <charleston.ak@gmail.com>
2024-06-18 05:39:17 -0600

    Temple Album

---
+++ scripts/temples.js
@@ -0,0 +1,20 @@
+const hambmenu = document.querySelector("#menu"); 
+const navmenu = document.querySelector(".mob-menu"); 
+
+hambmenu.addEventListener("click", () => {
+    hambmenu.classList.toggle("show"); 
+    navmenu.classList.toggle("show"); 
+    
+})
+
+navlinks = document.querySelector(".mob-menu");
+navlinks.addEventListener("click", () => {
+    hambmenu.classList.remove("show");
+    navmenu.classList.remove("show");
+})
+
+const last_modify = document.querySelector(".lmf");
+const currentYear = document.querySelector("#currentYear"); 
+last_modify.textContent = document.lastModified;
+today = new Date(); 
+currentYear.innerHTML = `&copy; <span>${today.getFullYear()} 🌴 Charles Alfred Koenig 🌴 Nicosia, Cyprus.</span>`;
