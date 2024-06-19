38c9356fee63619673ee5fa14ac1154d03521f86
kacharles <charleston.ak@gmail.com>
2024-06-19 09:42:20 -0600

    Modify Home Page

---
+++ scripts/base.js
@@ -0,0 +1,19 @@
+const lmf = document.querySelector("#lmf");
+const year = document.querySelector("#year");
+lmf.textContent = document.lastModified;
+today = new Date(); 
+year.innerHTML = `&copy; ${today.getFullYear()} 🌴 Charles Alfred Koenig 🌴 Nicosia, Cyprus.`;
+
+// Mobile menu styling..... 
+
+const menuBtn = document.querySelector("#burger_menu");
+const displayNav = document.querySelector(".mobile-menu");
+const line = document.querySelector("#line")
+const element = document.querySelector("hr");
+
+menuBtn.addEventListener("click", () => { 
+    // alert("menu button has been click");
+    menuBtn.classList.toggle("show");
+    displayNav.classList.toggle("show");
+});
+
--- scripts/getdate.js
+++
@@ -1,8 +0,0 @@
-const lmf = document.querySelector("#lmf");
-const get_year = document.querySelector("#current_year");
-lmf.textContent = document.lastModified;
-
-today = new Date(); 
-
-get_year.innerHTML = `&copy; <span>${today.getFullYear()} 🌴 Charles Alfred Koenig 🌴 Nicosia, Cyprus.</span>`;
-// last_modify.innerHTML = `Last Modified: <span>${document.lastModified}</span>`;
